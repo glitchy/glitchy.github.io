620bd39bb2bc28dd7a5436f13518a966f96f4ecb
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2018-11-11 16:16:59 -0700

    new and cleaner stuff.

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,18 @@
 
+//jquery watch for each time the mouse enters an element with the closs .target
+$( ".target" ).mouseenter(function() {
+    //$(this) select the element under mouse
+    //.prev() selects the previous sibling
+      $(this).prev() .addClass("border");
+    });
+    
+    //jquery watch for each time the mouse leaves an element with the closs .target
+    $( ".target" ).mouseleave(function() {
+    //$(this) select the element under mouse
+    //.prev() selects the previous sibling
+      $(this).prev().removeClass("border");
+    });
+
 var glow = $('.confirm_selection');
 setInterval(function(){
     glow.toggleClass('glow');
