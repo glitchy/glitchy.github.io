729f576744c30010ae5a5593065f1f67f99693f9
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2018-11-12 13:43:52 -0700

    deleted hover and touch events on JS.

---
+++ JS/splash.js
@@ -0,0 +1,35 @@
+$('#splash-nav').hover(function() {
+  $('#target').addClass("hvr-bob");
+})
+
+function hasTouch() {
+  return 'ontouchstart' in document.documentElement
+         || navigator.maxTouchPoints > 0
+         || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all :hover stylesheets
+  try { // prevent exception on browsers not supporting DOM styleSheets properly
+      for (var si in document.styleSheets) {
+          var styleSheet = document.styleSheets[si];
+          if (!styleSheet.rules) continue;
+
+          for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+              if (!styleSheet.rules[ri].selectorText) continue;
+
+              if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                  styleSheet.deleteRule(ri);
+              }
+          }
+      }
+  } catch (ex) {}
+}
+
+// $(document).ready(function() {     
+//     $('#second, #third').hover(function(){     
+//         $('#first').addClass('blue');    
+//     },     
+//     function(){    
+//         $('#first').removeClass('blue');     
+//     });
+// }); 
