afce51961711e070aa4976ae219f5de257197c64
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2018-11-11 18:29:44 -0700

    changed splash page back to glow effect.

--- JS/index.js
+++ JS/index.js
@@ -3,17 +3,17 @@
 $( ".target" ).mouseenter(function() {
     //$(this) select the element under mouse
     //.prev() selects the previous sibling
-      $(this).prev() .addClass("border");
+      $(this).prev().css("box-shadow", "0px 0px 5px #ddd");
     });
     
     //jquery watch for each time the mouse leaves an element with the closs .target
     $( ".target" ).mouseleave(function() {
     //$(this) select the element under mouse
     //.prev() selects the previous sibling
-      $(this).prev().removeClass("border");
+      $(this).prev().css("box-shadow", "0px 0px 5px #ddd");
     });
 
 var glow = $('.confirm_selection');
 setInterval(function(){
-    glow.toggleClass('glow');
+    glow.toggleClass('neon4');
 }, 1000);
